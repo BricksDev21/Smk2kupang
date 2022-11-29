7661521e9eaddce1c1f0dab61a6e1c869d092488
Whitnu12 <whitnu@gmail.com>
2022-11-29 12:11:47 +0700

    Install Tailwindcss Done

--- resources/views/check-tailwind.blade.php
+++ resources/views/check-tailwind.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
 
   <!-- CDN -->
   <!-- <script src="https://cdn.tailwindcss.com"></script>
